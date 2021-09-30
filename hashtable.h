35c3224ae6dd5d646d6e4060f9a1d4203e37da73
Praise1324 <tkiliwnik01@gmail.com>
2021-09-30 17:58:46 +1000

    Implemented hash table.\nCreated functions to interact with hash table

--- hashtable.h
+++ hashtable.h
@@ -1,22 +1,19 @@
-#include <inttypes.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>  
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
 
-// Item for hash table entries
+
+
+// Item for hash table
 typedef struct item item_t;
 struct item
 {
-    char *key;
-    int value;
+    u_int8_t *value;
     item_t *next;
 };
-
-void item_print(item_t *i)
-{
-    printf("key=%s value=%d", i->key, i->value);
-}
 
 // A hash table mapping a string to an integer.
 typedef struct htab htab_t;
@@ -26,10 +23,7 @@ struct htab
     size_t size;
 };
 
-// Initialise a new hash table with n buckets.
-// pre: true
-// post: (return == false AND allocation of table failed)
-//       OR (all buckets are null pointers)
+// Init hash table
 bool htab_init(htab_t *h, size_t n)
 {
     
@@ -40,15 +34,21 @@ bool htab_init(htab_t *h, size_t n)
 }
 
 // The Bernstein hash function.
-// A very fast hash function that works well in practice.
+// Modified to only use 6 character strings (reliabillity)
 size_t djb_hash(char *s)
 {
+    // ensure string formats are standardized
+    int input[7];
+    for (size_t i = 0; i < 6; i++)
+    {
+        input[i] = s[i];
+    }
     size_t hash = 5381;
     int c;
-    while ((c = *s++) != '\0')
+    for (size_t i = 0; i < 6; i++)
     {
         // hash = hash * 33 + c
-        hash = ((hash << 5) + hash) + c;
+        hash = ((hash << 5) + hash) + input[i];
     }
     return hash;
 }
@@ -59,61 +59,102 @@ size_t htab_index(htab_t *h, char *key)
     return djb_hash(key) % h->size;
 }
 
-// Find pointer to head of list for key in hash table.
-item_t *htab_bucket(htab_t *h, char *key)
-{
-    return h->buckets[htab_index(h,key)];
-}
-
-// Find an item for key in hash table.
-// pre: true
-// post: (return == NULL AND item not found)
-//       OR (strcmp(return->key, key) == 0)
-item_t *htab_find(htab_t *h, char *key)
-{
-    item_t *current = htab_bucket(h, key);
-
-    for (; current != NULL; current = current->next){
-        if (strcmp(current->key, key) == 0) {
-            return current;
-        }
+// Convert the key into an array of ints
+// perams: key must be a string of 6 characters
+u_int8_t* get_val(char* key){
+    u_int8_t *temp = calloc(6, sizeof(u_int8_t));
+    for (int i = 0; i < 6; i++)
+    {
+        temp[i] = (u_int8_t)key[i];
     }
-
-    return NULL;
+    return temp;
 }
 
-// Add a key with value to the hash table.
-// pre: htab_find(h, key) == NULL
-// post: (return == false AND allocation of new item failed)
-//       OR (htab_find(h, key) != NULL)
-bool htab_add(htab_t *h, char *key, int value)
-{
-    // TODO: implement this function
-    item_t *head = htab_bucket(h, key);
+// Add a value into hasfh table
+bool htab_add(htab_t *h, char *key){
+    // get pointer to head of bucket list
+    item_t *head = h->buckets[(djb_hash(key) % h->size)];
 
     item_t *new_node = (item_t *)malloc(sizeof(item_t));
     if (new_node == NULL){
         return false;
     }
 
-    size_t bucket_idx = htab_index(h, key);
+    new_node->value = get_val(key);
+    new_node->next = h->buckets[htab_index(h, key)];
+    h->buckets[htab_index(h, key)] = new_node;
+}
 
-    new_node->key = key;
-    new_node->value = value;
-    new_node->next = h->buckets[bucket_idx];
-    h->buckets[bucket_idx] = new_node;
 
+// Read through a file of numberplates and insert into hash table
+// conditions:
+//     input: a text file consisting of lines of exactly 6 characters
+void read_plates(const char *input, htab_t *h){
+    FILE* text  = fopen(input, "r");
+    char line[10];
+    
+    while (NULL != fgets(line, sizeof(line), text))
+    {
+        htab_add(h, line);
+    }
+}
+
+// Recursive function to search a bucket in a has table for a value
+// conditions:
+//     input: must be more than 6 characters to prevent undefined behavior
+//     input: only first 6 characters will be checked        
+bool search_bucket(item_t *item, u_char *input){
+    for (size_t i = 0; i < 6; i++)
+    {
+        if (input[i] != item->value[i])
+        {
+            if (item->next == NULL)
+            {
+                // end of bucket with no match
+                return false;
+            } else{
+                // search next bucket
+                return search_bucket(item->next, input);
+            }
+        }
+    }
+    // search successfull
     return true;
 }
 
-// Print the hash table.
-// pre: true
-// post: hash table is printed to screen
+// Determine if an input is stored in the hash table
+bool search_plate(htab_t *h, u_char *input){
+    // determine bucket result would be stored in
+    item_t *head = h->buckets[(djb_hash(input) % h->size)];
+    if (head == NULL)
+    {
+        return false;
+    } else{
+        return search_bucket(head, input);
+    }
+}
+
+
+
+
+// for debugging only
+// print an item from a hash table
+void item_print(item_t *it)
+{
+    printf("value= ");
+    for (size_t i = 0; i < 6; i++)
+    {
+        printf("%c", it->value[i]);
+    }
+}
+
+// for debugging only
+// print the contents of a hash table
 void htab_print(htab_t *h)
 {
     printf("hash table with %d buckets\n", h->size);
     for (size_t i = 0; i < h->size; ++i)
-    {
+    {//for debugging only
         printf("bucket %d: ", i);
         if (h->buckets[i] == NULL)
         {
@@ -134,88 +175,20 @@ void htab_print(htab_t *h)
     }
 }
 
-// Delete an item with key from the hash table.
-// pre: htab_find(h, key) != NULL
-// post: htab_find(h, key) == NULL
-void htab_delete(htab_t *h, char *key)
-{
-    item_t *head = htab_bucket(h, key);
-    item_t *current = head;
-    item_t *previous = NULL;
-    while (current != NULL)
+// for debugging only
+// check of a plate is in the hash table
+// pre: max input size is 9 chars
+void htab_check(htab_t *h){
+    char input[7];
+    printf("Please enter a value to search\n");
+    scanf("%s", &input);
+    if (search_plate(h, input))
     {
-        if (strcmp(current->key, key) == 0)
-        {
-            if (previous == NULL)
-            { // first item in list
-                h->buckets[htab_index(h, key)] = current->next;
-            }
-            else
-            {
-                previous->next = current->next;
-            }
-            free(current);
-            break;
-        }
-        previous = current;
-        current = current->next;
-    }
-}
-
-// Destroy an initialised hash table.
-// pre: htab_init(h)
-// post: all memory for hash table is released
-void htab_destroy(htab_t *h)
-{
-    for (int i = 0; i < h->size; i++){
-        item_t *current = h->buckets[i];
-
-        while (current != NULL)
-        {
-            item_t *next = current->next;
-            free(current);
-
-            current = next;
-        }
-        
+        printf("%s was found in the hash table\n", input);
+    } else{
+        printf("%s was not found in the hash table\n", input);
     }
 
-    free(h->buckets);
-    h->buckets = NULL;
-    h->size = 0;
-}
-
-int main(int argc, char **argv)
-{
-    // create a hash table with 10 buckets
-    printf("creating hash table:\n");
-    size_t buckets = 10;
-    htab_t h;
-    if (!htab_init(&h, buckets))
-    {
-        printf("failed to initialise hash table\n");
-        return EXIT_FAILURE;
-    }
-
-    // add items to hash table and preint
-    htab_add(&h, "hello", 1);
-    htab_add(&h, "hello", 1); // violate pre-condition to get two hello's in table
-    htab_add(&h, "world", 2);
-    htab_print(&h);
-    printf("\n");
-
-    // find item in hash table
-    printf("find item in hash table:\n");
-    item_print(htab_find(&h, "hello"));
-    printf("\n\n");
-
-    // delete items from hash table and print
-    printf("deleting items from hash table:\n");
-    htab_delete(&h, "hello");
-    htab_delete(&h, "world");
-    htab_print(&h);
-    printf("\n");
-
-    // clean up hash table
-    htab_destroy(&h);
+    printf("%s hashes to %d", input, (djb_hash(input) % h->size));
+    
 }
--- management_system.c
+++ management_system.c
@@ -1,1 +1,67 @@
+#include <fcntl.h>
+#include <semaphore.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "hashtable.h"
+#include "shm.h"
+
+#define SHM_SIZE 1440
+#define PLATES_FILE "Plates.txt"
+#define HASHTABLE_BUCKETS 100
+
+
+bool create_shared_object( shared_memory_t* shm, const char* share_name ) {
+    // Remove any previous instance of the shared memory object, if it exists.
+    shm_unlink(shm->name);
+
+    // Assign share name to shm->name.
+    shm->name = share_name;
+
+    // Create the shared memory object, allowing read-write access, and saving the
+    // resulting file descriptor in shm->fd. If creation failed, ensure 
+    // that shm->data is NULL and return false.
+    if ((shm->fd =shm_open(shm->name, O_CREAT | O_RDWR, 0666)) < 0)
+    {
+        shm->data = NULL;
+        return false;
+    }
+    
+
+    // Set the capacity of the shared memory object via ftruncate. If the 
+    // operation fails, ensure that shm->data is NULL and return false. 
+    if (ftruncate(shm->fd, SHM_SIZE) != 0){
+        shm->data = NULL;
+        return false;
+    }
+
+    // Otherwise, attempt to map the shared memory via mmap, and save the address
+    // in shm->data. If mapping fails, return false.
+    if ((shm->data = mmap(0, SHM_SIZE, PROT_WRITE, MAP_SHARED, shm->fd, 0)) == MAP_FAILED){
+        return false;
+    }
+
+    // shared memory creation successful
+    return true;
+}
+
+void main(){
+
+    // Setup for hash table and insert file contents
+    htab_t hasht;
+    size_t buckets = HASHTABLE_BUCKETS;
+    htab_init(&hasht, buckets);
+    read_plates(PLATES_FILE, &hasht);
+
+    // For testing, print hash table and contents
+    htab_print(&hasht);
+    htab_check(&hasht);
+
+}
 

4939ed2b05bf8770dc2bf65595c042d39ae3392e
dulce-acevedo-QUT <dulcedeyanira.acevedo@connect.qut.edu.au>
2021-10-04 16:30:44 +1000

    Shared memory structs for the car park simulator

--- simulator.c
+++ simulator.c
@@ -1,1 +1,90 @@
+pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <pthread.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+
+pthread_mutex_t cp_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cp_condvar = PTHREAD_COND_INITIALIZER;
+
+#define LEVELS 5
+#define ENTRANCES 5
+#define EXITS 5
+
+typedef struct license_plate_recognition_sensor{
+
+    pthread_mutex_t lock;
+	pthread_cond_t condition_variable;
+    char license_plate;
+
+} LPR;
+
+typedef struct park_boom_gates{
+
+    pthread_mutex_t lock;
+	pthread_cond_t condition_variable;
+    char status;
+
+} boom_gate;
+
+typedef struct information_sign{
+
+    pthread_mutex_t lock;
+	pthread_cond_t condition_variable;
+    char info;
+
+} info_sign;
+
+typedef struct temperature_sensors{
+
+    int16_t current_temperature;
+
+} temp_sensor;
+
+typedef struct fire_alarms{
+
+    char status;
+
+} fire_alarm;
+
+typedef struct park_entrance{
+    
+    LPR lpr_EN; 
+    boom_gate boom_gate_EN;
+    info_sign sign;
+    
+} entrance_s;
+
+typedef struct park_exit{  
+
+    LPR lpr_EX;
+    boom_gate boom_gate_EX;
+
+} exit_s;
+
+
+
+typedef struct level{
+
+    LPR lpr_LVL;
+    temp_sensor ts;
+    fire_alarm alarm;
+    
+} level;
+
+/**
+ * A shared data block.
+ */
+typedef struct shared_memory{
+
+    entrance_s ent;
+    exit_s ext;
+
+} shared_memory_t;
+
 

6a1296dee88a3ca947140e9ffd83e62cee8e1bcf
jarrahjarrah <n8505926@qut.edu.au>
2021-10-04 20:13:33 +1000

    test commit

--- management_system.c
+++ management_system.c
@@ -1,1 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <pthread.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
 
+//status of the LPR sensors 
+//and keep track of where each car is in the car park
+
+
+
+//tell the boomgates when to open and when to close
+
+
+
+//control what is displayed on the information signs at each entrance
+void entranceDisplay (int entranceNo) {
+
+}
+
+
+//Ensure there is room in the car park before allowing new vehicles in
+int findAvailableSpace () {
+    //need to lock the space so if there is only 1 space left and
+    //any new cars come along, they wont be allocated the same space 
+    
+    //return the level that the car needs to park on
+    return 0;
+}
+
+
+//keep track of how long each car has been in the parking
+//and produce a bill once the car leaves
+void timeSpent (int timeEntered, int timeExited){
+    //    
+}
+
+
+
+//display the current status of the parking lot on a frequently
+//updating screen, showing how full each level is, the current
+//status of the boom gates, signs, temperature sensors and alarms, 
+//as well as the revenue the car park has brought in so far
+void displayScreen () {
+    //get the status of each level and display how full the level is
+    printf("----------------------------------------\n");
+    printf("~Level 1~\n");
+    printf("Capacity: %d/20\n", 2);
+    
+    printf("~Level 2~\n");
+    printf("Capacity: %d/20\n", 12);
+    
+    printf("~Level 3~\n");
+    printf("Capacity: %d/20\n", 4);
+}
+
+int main() {
+    displayScreen();
+    return 0;
+}
--- simulator.c
+++ simulator.c
@@ -1,1 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <pthread.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define LEVELS 5
+#define ENTRANCES 5
+#define EXITS 5
+//Simulate car
+typedef struct Cars {
+    const char* rego;
+    int level;
+    int timeEntered;
+
+}Car;
+typedef struct Levels {
+    int space;
+
+}Level;
+typedef enum boomGateState {
+    opened, closed, rising, lowering
+} bg_state;
+typedef enum { false, true } bool;
+
+void simulate() {
+    bool space;
+    Level level1; 
+    level1.space = 12;
+    Car car1;
+    car1.rego = "123456";
+    bg_state boom1;
+    space = true;
+    //read rego
+    //dertermine if they can enter or not
+    while(space == true) {
+        boom1 = closed;
+        switch(boom1) {
+            case closed: printf("The boom gate is closed.\n") ;
+        }
+        
+        if(car1.rego == "123456") { //check if the number plate matches one in the list
+            printf("The car is allowed access.\n");
+
+            //is there any space available?
+            if(level1.space < 20) {
+                space = true;
+                //open boom gate
+                boom1 = opened;
+                printf("There is space for this car.\n");
+            }
+            else {
+                space = false;
+                printf("There is no space for this car.\n");
+            }
+        }
+    
+        //allocate the space to the car
+        if(space == true) {
+            car1.level = 1;
+            level1.space = level1.space + 1;
+            };
+        //add car to level's list of cars
+        
+        printf("Level 1's space filled in now: %d\n", level1.space);
+        switch(boom1) {
+            case opened: printf("The boom gate is closed.\n") ;
+        }
+    }
+    
+    //
+}
+
+//Simulate boom gates
+
+
+//Simulate temperature
+int main() {
+    simulate();
+    return 0;
+}
+
 

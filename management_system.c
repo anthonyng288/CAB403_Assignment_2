7fc59eeda9c2dda35233ef8277f9405eb4ad4fe1
dulce-acevedo-QUT <dulcedeyanira.acevedo@connect.qut.edu.au>
2021-10-22 17:43:03 +1000

    Car park space in progress

--- management_system.c
+++ management_system.c
@@ -16,6 +16,7 @@
 #define PLATES_FILE "Plates.txt"
 #define HASHTABLE_BUCKETS 100
 
+
 // Global variables
 shared_mem_t shm;
 
@@ -166,6 +167,21 @@ bool search_plate(htab_t *h, u_char *input){
 }
 
 /*
+
+//Ensures that there is room in the car park before
+//allowing new vehicles in (number of cars < number of levels * the number of cars per level).
+
+bool cp_has_space (int num_cars){
+
+    if num_cars < (LEVELS * LEVEL_CAPACITY){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+//Chooses a random available level
 char available_level(){
     
 }
@@ -176,23 +192,26 @@ char available_level(){
 // input: if the plate is or not allowed in
 // input: if the carpark has space or not
 char entry_message( bool search_plate, bool cp_has_space){
+    
+    //
     if(cp_has_space){
         if (search_plate){
             //show a character between ‘1’ and ‘5’ 
             //floor the driver should park on
-            
+            return available_level();
 
         }
         else{
             //If the driver is unable to access the car park due to not being in the access file, the
             //sign will show the character ‘X’
-            return X;
+            ;
+            return 'X';
         }
     }
     else{
         // If the driver is unable to access the car park due to it being full, the sign will show the
         //character ‘F
-        return F;
+        return 'F';
     }
 }
 

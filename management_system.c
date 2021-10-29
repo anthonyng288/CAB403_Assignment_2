a49f75d3a28a86f004480c4671aca44076b5dc0a
jarrahjarrah <n8505926@qut.edu.au>
2021-10-29 11:55:44 +1000

    Revert "car billing and calculate revenue"
    This reverts commit b7550f281d99897a87c1613c74559c7705db4a1a.

--- management_system.c
+++ management_system.c
@@ -63,54 +63,6 @@ void bill_car(car_t* car){
     return;
 }
 
-// Calculates the bill for a car when they trigger the exit LPR
-void bill_car(car_t* car){
-    // Open billing.txt in append mode
-    FILE* fp = fopen(BILLING_FILE, "a");
-
-    // Calculate the difference between car->enter_time 
-    // and exit_time
-    time_t exit_time = time(0) * 1000;
-    double total_time = difftime(exit_time, car->enter_time);
-
-    // Calculate total bill from time_spent in the car park
-    double bill = total_time * 0.05;
-    
-    // Write to billing.txt file
-    fprintf(fp,"%s $%.02f\n", car->lp, bill);
-    fclose(fp);
-}
-
-// Calculates the total revenue for the car park
-double calculate_total_revenue() {
-    FILE *fp;
-    char * line = NULL;
-    size_t len = 0;
-    ssize_t read;
-    double revenue = 0.0;
-    
-    if((fp = fopen(BILLING_FILE, "r")) != NULL) {
-        while ((read = getline(&line, &len, fp)) != -1) {
-            char money_str[len];
-            double money_dbl;
-            char *eptr;
-
-            // Get substring of each car's bill total
-            strncpy(money_str, line+8, len);
-
-            // Convert string to double
-            money_dbl = strtod(money_str, &eptr);
-            revenue += money_dbl; 
-        }  
-        fclose(fp);
-        return revenue;  
-    }
-    else {
-        printf("File does not exist.");
-        return 0;
-    }    
-}
-
 
 
 
